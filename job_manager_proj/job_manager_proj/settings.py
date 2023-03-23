962bd874c934c097f136313a57d9b0e60778c377
Alexandr <maximchik.alexandr@yandex.ru>
2023-03-23 21:45:36 +0300

    Production host is added to settings.py

--- job_manager_proj/job_manager_proj/settings.py
+++ job_manager_proj/job_manager_proj/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(env("DEBUG"))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env("PRODUCTION_HOST")]
 
 # Application definition
 
