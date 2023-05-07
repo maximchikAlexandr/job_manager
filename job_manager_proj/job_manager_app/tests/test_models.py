30229a4ac23d889b33226c428a9484579caed172
Alexandr <maximchik.alexandr@yandex.ru>
2023-05-07 23:12:47 +0300

    Add pytest and replace paths of the fixtures

---
+++ job_manager_proj/job_manager_app/tests/test_models.py
@@ -0,0 +1,28 @@
+import json
+
+from django.test import TestCase
+
+from job_manager_app.migrations import fixtures as fixs
+from job_manager_app.models import (
+    ActOfCompletedWork,
+    Company,
+    Employee,
+    Month,
+    MonthJob,
+    ServiceAgreement,
+    TypeOfJobs,
+)
+
+
+class UsersTestCase(TestCase):
+    fixtures = fixs
+
+    def setUp(self) -> None:
+        self.valid_data = []
+        for fixture in self.fixtures:
+            with open(fixture, "r", encoding="utf-8") as fixture_file:
+                data = json.load(fixture_file)
+                self.valid_data.extend(data)
+
+    def test_some(self):
+        assert True
