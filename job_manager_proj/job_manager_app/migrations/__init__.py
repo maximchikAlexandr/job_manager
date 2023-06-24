8586d025995130043067de3a607a3b75d99db88a
Alexandr <maximchik.alexandr@yandex.ru>
2023-06-24 20:44:37 +0300

    Add new models

--- job_manager_proj/job_manager_app/migrations/__init__.py
+++ job_manager_proj/job_manager_app/migrations/__init__.py
@@ -1,4 +1,3 @@
 from django.conf import settings
 
-fixtures = (settings.BASE_DIR / "job_manager_app/migrations/fixtures/catalog_fixture.json",
-            settings.BASE_DIR / "job_manager_app/migrations/fixtures/test_fixture.json")
+fixtures = (settings.BASE_DIR / "job_manager_app/migrations/fixtures/catalog_fixture.json",)
