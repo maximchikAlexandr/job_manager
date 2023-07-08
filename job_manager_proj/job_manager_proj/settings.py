40afb95140e3f2c294b9b80a63da4ab1bbcc9ac0
Alexandr <maximchik.alexandr@yandex.ru>
2023-07-08 21:26:27 +0300

    Add integration with CRM Bitrix24

--- job_manager_proj/job_manager_proj/settings.py
+++ job_manager_proj/job_manager_proj/settings.py
@@ -151,8 +151,15 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGGING = {
     "version": 1,
-    "handlers": {"console": {"class": "logging.StreamHandler"}},
-    "loggers": {"django.db.backends": {"handlers": ["console"], "level": "DEBUG"}},
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"}
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "DEBUG"
+        }
+    }
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
